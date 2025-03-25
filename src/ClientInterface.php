b47ef29c7aceac17dcb14563c975c4d03cdca8c7
Borisz Juhasz <borisz.juhasz@emarsys.com>
2025-03-25 16:48:05 +0100

    SECURITY-OPS: add missing method
    Co-authored-by: Dora Kaszasne Sztanko <dora.sztanko@emarsys.com>

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -7,4 +7,6 @@ interface ClientInterface
     public function isValid(string $id): bool;
 
     public function filterInvalid(array $msids): array;
+
+    public function getSessionData(string $sessionDataToken): array;
 }
